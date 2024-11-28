8bfdb9827d2e5bbb773bb473c9d52e989d620d4e
rahul-cah <rahul.rahul@cardinalhealth.com>
2024-11-28 16:39:18 +0530

    connecting to DEV Env

--- src/main/java/com/cardinalhealth/nphs_consumer_app/Consumer.java
+++ src/main/java/com/cardinalhealth/nphs_consumer_app/Consumer.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Consumer {
-    @KafkaListener(topics= {"nphs_isotrac_sample"}, groupId="spring-boot-kafka")
+    @KafkaListener(topics= {"NPHS_ISOTRAC_INVOICE"}, groupId="nphs_isotrac_sample")
     public void consume(String quote) {
         System.out.println("received= " + quote);
     }
